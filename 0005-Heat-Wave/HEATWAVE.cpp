4e10fd2d73ea26543eef84267d43e9e1e5deb2cc
mdsaqlain538 <mdsaqlainpatel538@gmail.com>
2024-06-15 03:52:21 -0700

    code chef heat wave

---
+++ 0005-Heat-Wave/HEATWAVE.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int x_degree, y_degree;
+	cin>>x_degree>>y_degree;
+	
+	if(x_degree >= y_degree){
+	    cout<<"NO"<<endl;
+	}else{
+	    cout<<"YES"<<endl;
+	}
+}
